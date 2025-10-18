dd2961f8da9f03d6c125f77810399085c97a5e6b
Roberto De Gaetano <roberto@robertodegaetano.it>
2025-10-18 11:11:49 +0200

    Update const.py

---
+++ PYTHON/FLASK/Users/const.py
@@ -0,0 +1,2 @@
+db_config = {"host": "localhost", "user": "root", "password": "", "database": "utils"}
+path_list = {"root_path": "/", "search_path": "/search", "addition_path": "/add", "removal_path": "/del"}
