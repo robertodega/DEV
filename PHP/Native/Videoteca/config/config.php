196a4ce2ad55e31d993da2952b37959e1c7a1dcd
Roberto <roberto@robertodegaetano.it>
2025-11-13 15:49:59 +0100

    update

--- PHP/Native/Videoteca/config/config.php
+++
@@ -1,5 +0,0 @@
-<?php
-    define('DB_HOST','localhost');
-    define('DB_NAME','videoteca');
-    define('DB_USER','root');
-    define('DB_PASS','');
--- PHP/Smarty/Rubrica/include/classConn.php
+++
@@ -1,8 +0,0 @@
-<?php
-    include_once 'classes/dbman.php';
-    include_once 'classes/manager.php';
-
-    $db = new Dbman();
-    $conn = $db->getConn();
-    $manager = new Manager($conn);
-?>
