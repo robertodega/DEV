9d3951be4ecbb571cbffcca102e09164e5f26b1f
Roberto De Gaetano <roberto@robertodegaetano.it>
2025-12-29 14:36:04 +0100

    Update decorators.py

--- PYTHON/NATIVE/decorators.py
+++ PYTHON/NATIVE/decorators.py
@@ -1,1 +1,27 @@
 
+import os
+os.system('cls' if os.name == 'nt' else 'clear')
+
+#   ******************************* DECORATOR *******************************
+
+def decorator(func):
+    def wrapper():
+        print("\nStarting execution ... ")
+        func()
+        print("Execution is ended ... \n")
+    return wrapper()
+
+def say_hello():
+    print("Hello World!!!")
+
+def greetings():
+    print("Thank you all!")
+
+decorator(say_hello)
+decorator(greetings)
+
+#   @decorator
+#   def say_hello_with_decorator():
+#       print("Hello World!!!")
+#   
+#   say_hello_with_decorator()
