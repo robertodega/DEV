422e6aa7f0613e7a460032101658da8f13b81f73
Roberto De Gaetano <roberto@robertodegaetano.it>
2026-02-13 09:57:18 +0100

    Create files_organizer.py to organize image files
    This script organizes image files by renaming them with a daily timestamp and a sequential prefix. It creates an 'images' directory if it doesn't exist and moves JPG files into this directory with the new naming convention.

---
+++ files_organizer.py
@@ -0,0 +1,57 @@
+import os
+import datetime
+
+os.system('cls')
+
+def path_definition():
+    images_dir = "images"
+    path_content = {"images": images_dir}
+    return path_content
+
+def check_double_digit(string):
+    if len(str(string)) == 1:
+        prefix = "0" + str(string)
+    else:
+        prefix = str(string)
+    return prefix
+
+def daily_name_definition():
+    return str(datetime.date.today().year) + "_" + str(check_double_digit(datetime.date.today().month)) + "_" + str(check_double_digit(datetime.date.today().day))
+
+def arrange_files(files, ext):
+    path_content = path_definition()
+    files_with_ext = [file for file in files if file.endswith(ext)]
+    daily_name = daily_name_definition()
+
+    if ext == 'jpg':
+        if not(os.path.exists(f"{str(path_content["images"])}")):
+            os.mkdir(str(path_content["images"]))
+        for i, file in enumerate(files_with_ext):
+            
+            prefix = check_double_digit(i+1)
+
+            if not(os.path.exists(f"{str(path_content["images"])}/{daily_name}_{prefix}.{ext}")):
+                new_file_name = f"{str(path_content["images"])}/{daily_name}_{prefix}.{ext}"
+            else:
+                current_prefix_value = len(os.listdir(f"{path_content["images"]}"))
+                prefix = int(current_prefix_value) + 1
+                prefix = check_double_digit(prefix)
+                new_file_name = f"{str(path_content["images"])}/{daily_name}_{prefix}.{ext}"
+
+            os.rename(file, f"{new_file_name}")
+
+        print(f"\n\nCurrent content of {str(path_content["images"])} folder:")
+        for file in os.listdir(f"{str(path_content["images"])}"):
+            print(f"\t{file}")
+
+if __name__ == "__main__":
+    files = os.listdir()
+    arrange_files(files, 'jpg')
+
+
+#   TEST
+#   rm -rf images
+#   touch 12.jpg 34.jpg 56.jpg 78.png
+#   python3 files_organizer.py
+#   mv 78.png 78.jpg
+#   python3 files_organizer.py
